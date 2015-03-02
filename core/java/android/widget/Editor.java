669e8116379196e9574f26823852acc6bc361aea
Clara Bayarri <clarabayarri@google.com>
2015-03-02 16:09:09 +0000

    Move the "Replace" popup option to the Text Selection ActionMode.
    As a first step in unifying the cut/copy/paste ActionMode in Editor
    with the paste/replace popup, I'm moving the replace option to
    the CAB ActionMode. Paste is already there, so for now all options
    are together.
    
    Missing things to address in upcoming CLs:
    - Invoke the ActionMode in all cases where the popup shows up now,
    ensuring only the options that are currently available show up.
    - Get rid of the current popup
    - Make the ActionMode a floating toolbar (pending feature completion)
    - Define a keyboard shortcut for replace?
    
    Note that since the ActionMode still shows up in the ActionBar and
    replace has no icon it now appears as text and takes up lots of
    space. This will improve when we can switch to using a floating
    toolbar.
    
    Change-Id: Ib6b60bae9b58e4db96b9c4cee556e19d3f1bb466

--- core/java/android/widget/Editor.java
+++ core/java/android/widget/Editor.java
@@ -2971,6 +2971,12 @@ public class Editor {
                                 MenuItem.SHOW_AS_ACTION_ALWAYS | MenuItem.SHOW_AS_ACTION_WITH_TEXT);
             }
 
+            if (mTextView.isSuggestionsEnabled() && isCursorInsideSuggestionSpan()) {
+                menu.add(0, TextView.ID_REPLACE, 0, com.android.internal.R.string.replace).
+                        setShowAsAction(
+                                MenuItem.SHOW_AS_ACTION_ALWAYS | MenuItem.SHOW_AS_ACTION_WITH_TEXT);
+            }
+
             styledAttributes.recycle();
 
             if (mCustomSelectionActionModeCallback != null) {
@@ -3002,6 +3008,10 @@ public class Editor {
                  mCustomSelectionActionModeCallback.onActionItemClicked(mode, item)) {
                 return true;
             }
+            if (item.getItemId() == TextView.ID_REPLACE) {
+                onReplace();
+                return true;
+            }
             return mTextView.onTextContextMenuItem(item.getItemId());
         }
 
@@ -3028,6 +3038,13 @@ public class Editor {
 
             mSelectionActionMode = null;
         }
+    }
+
+    private void onReplace() {
+        int middle = (mTextView.getSelectionStart() + mTextView.getSelectionEnd()) / 2;
+        stopSelectionActionMode();
+        Selection.setSelection((Spannable) mTextView.getText(), middle);
+        showSuggestions();
     }
 
     private class ActionPopupWindow extends PinnedPopupWindow implements OnClickListener {
@@ -3088,10 +3105,7 @@ public class Editor {
                 mTextView.onTextContextMenuItem(TextView.ID_PASTE);
                 hide();
             } else if (view == mReplaceTextView) {
-                int middle = (mTextView.getSelectionStart() + mTextView.getSelectionEnd()) / 2;
-                stopSelectionActionMode();
-                Selection.setSelection((Spannable) mTextView.getText(), middle);
-                showSuggestions();
+                onReplace();
             }
         }
 
