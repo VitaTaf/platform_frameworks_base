82e317fafed820b6719d25b056b61a0d0c0547e2
Clara Bayarri <clarabayarri@google.com>
2015-03-02 19:42:48 +0000

    Add a type parameter to startActionMode() calls.
    This requires adding a new method to View and Window.Callback to pass
    down the type as a parameter.
    
    For compatibility purposes, the new method implementations keep the
    type and call the old method, in case clients have subclassed it.
    
    Change-Id: If5d857f131e33be8cc6a8814f2e9c4e85ad2da25

--- core/java/android/view/ViewRootImpl.java
+++ core/java/android/view/ViewRootImpl.java
@@ -6189,6 +6189,12 @@ public final class ViewRootImpl implements ViewParent,
     }
 
     @Override
+    public ActionMode startActionModeForChild(
+            View originalView, ActionMode.Callback callback, int type) {
+        return null;
+    }
+
+    @Override
     public void createContextMenu(ContextMenu menu) {
     }
 
