288ec1eed483960821596b74b5ccb32d8fad4fcc
Clara Bayarri <clarabayarri@google.com>
2015-04-10 15:35:31 +0100

    Editor: keep text selection when starting a process text intent.
    This CL makes sure the selection is not cleared when a text processing
    intent starts, so that we can properly replace the selection when it
    comes back.
    
    It also fixes a related issue with the selection handles. The previous
    code assumed a text selection mode starts from touch and enters drag
    mode by default. This CL makes the code conscious of whether it should
    show the selection cursors or enter drag mode.
    
    BUG: 20148456
    Change-Id: Ie8632e4a96f431dc6b2e4d0b4d3978eb55149172

--- core/java/android/widget/TextView.java
+++ core/java/android/widget/TextView.java
@@ -8965,6 +8965,7 @@ public class TextView extends View implements ViewTreeObserver.OnPreDrawListener
 
     void replaceSelectionWithText(CharSequence text) {
         ((Editable) mText).replace(getSelectionStart(), getSelectionEnd(), text);
+        mEditor.startSelectionActionModeWithSelection();
     }
 
     /**
