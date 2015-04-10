76fbb824f0c4106da6cb9b4c9f1d26723f48b6c9
Tim Kilbourn <tkilbourn@google.com>
2015-04-09 17:14:34 -0700

    Save search context when dispatching onSearchRequested.
    The InputDevice used to trigger the search is saved in a SearchEvent
    so an application implementing onSearchRequested determine the device
    triggering the search.
    
    Bug: 15374820
    Change-Id: If2cd14f77ce572a5ee131697cd63145fbea63f27

--- core/java/android/app/Dialog.java
+++ core/java/android/app/Dialog.java
@@ -47,6 +47,7 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.MotionEvent;
 import android.view.PhoneWindow;
+import android.view.SearchEvent;
 import android.view.View;
 import android.view.View.OnCreateContextMenuListener;
 import android.view.ViewGroup;
@@ -117,6 +118,8 @@ public class Dialog implements DialogInterface, Window.Callback,
     private static final int SHOW = 0x45;
 
     private Handler mListenersHandler;
+
+    private SearchEvent mSearchEvent;
 
     private ActionMode mActionMode;
 
@@ -984,6 +987,14 @@ public class Dialog implements DialogInterface, Window.Callback,
     /**
      * This hook is called when the user signals the desire to start a search.
      */
+    public boolean onSearchRequested(SearchEvent searchEvent) {
+        mSearchEvent = searchEvent;
+        return onSearchRequested();
+    }
+
+    /**
+     * This hook is called when the user signals the desire to start a search.
+     */
     public boolean onSearchRequested() {
         final SearchManager searchManager = (SearchManager) mContext
                 .getSystemService(Context.SEARCH_SERVICE);
@@ -997,6 +1008,17 @@ public class Dialog implements DialogInterface, Window.Callback,
         } else {
             return false;
         }
+    }
+
+    /**
+     * During the onSearchRequested() callbacks, this function will return the
+     * {@link SearchEvent} that triggered the callback, if it exists.
+     *
+     * @return SearchEvent The SearchEvent that triggered the {@link
+     *                    #onSearchRequested} callback.
+     */
+    public final SearchEvent getSearchEvent() {
+        return mSearchEvent;
     }
 
     /**
