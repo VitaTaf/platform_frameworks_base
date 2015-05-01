94dc9e3d93111a34a3cefa6865288df73ce8b3a8
Chris Craik <ccraik@google.com>
2015-05-01 14:22:47 -0700

    Revert "Revert "Merge getDisplayList and updateDisplayListIfDirty""
    Fixes breakage from HwAccelerationTest
    
    This reverts commit b2847afde24aac22c8fb804cdce0c24b8a7c40c4.
    
    Change-Id: I762b3c9020fc1d06bac61ffa8b956049147515b1

--- core/java/android/view/GhostView.java
+++ core/java/android/view/GhostView.java
@@ -41,7 +41,7 @@ public class GhostView extends View {
         final ViewGroup parent = (ViewGroup) mView.getParent();
         setGhostedVisibility(View.INVISIBLE);
         parent.mRecreateDisplayList = true;
-        parent.getDisplayList();
+        parent.updateDisplayListIfDirty();
     }
 
     @Override
@@ -49,7 +49,7 @@ public class GhostView extends View {
         if (canvas instanceof HardwareCanvas) {
             HardwareCanvas hwCanvas = (HardwareCanvas) canvas;
             mView.mRecreateDisplayList = true;
-            RenderNode renderNode = mView.getDisplayList();
+            RenderNode renderNode = mView.updateDisplayListIfDirty();
             if (renderNode.isValid()) {
                 hwCanvas.insertReorderBarrier(); // enable shadow for this rendernode
                 hwCanvas.drawRenderNode(renderNode);
@@ -84,7 +84,7 @@ public class GhostView extends View {
             final ViewGroup parent = (ViewGroup) mView.getParent();
             if (parent != null) {
                 parent.mRecreateDisplayList = true;
-                parent.getDisplayList();
+                parent.updateDisplayListIfDirty();
             }
         }
     }
