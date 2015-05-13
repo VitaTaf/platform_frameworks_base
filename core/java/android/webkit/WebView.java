6cc9a875a62315425149f0357ede4dbd10690162
Selim Gurun <sgurun@google.com>
2015-05-12 17:53:24 -0700

    Add a temporary wrapper for ViewStructure until Webview APK is updated
    It turned out that after the API change from ViewAssistStructure to
    ViewStructure, the suggested mechanism did not work, and webview
    started throwing abstractmethoderror exceptions. Temporarily
    solve the problem by wrapping ViewStructure inside a
    ViewAssistStructure. Once Webview APK is updated, drop it.
    
    Change-Id: I09dfe7dac9c2bc7c037d842844c61dd879629470

--- core/java/android/webkit/WebView.java
+++ core/java/android/webkit/WebView.java
@@ -2399,7 +2399,8 @@ public class WebView extends AbsoluteLayout
 
     @Override
     public void onProvideVirtualStructure(ViewStructure structure) {
-        mProvider.getViewDelegate().onProvideVirtualAssistStructure(structure);
+        ViewAssistStructure s = new ViewAssistStructure(structure);
+        mProvider.getViewDelegate().onProvideVirtualAssistStructure(s);
     }
 
     /** @hide */
