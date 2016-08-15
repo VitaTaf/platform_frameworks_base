SphinxOS/frameworks_base

Mutate scroll bar drawables
Bug: 19285726
Change-Id: I4bb596433c1fa1cc4e2fa53d0cdae992f9add858

Former-commit-id: f8512ce08174426dfbba96e8108e5a64332f1e71

core/java/android/widget/ScrollBarDrawable.java
@@ -44,6 +44,7 @@ public class ScrollBarDrawable extends Drawable {
     private final Rect mTempBounds = new Rect();
     private boolean mAlwaysDrawHorizontalTrack;
     private boolean mAlwaysDrawVerticalTrack;
+    private boolean mMutated;
 
     public ScrollBarDrawable() {
     }
@@ -191,6 +192,9 @@ public class ScrollBarDrawable extends Drawable {
 
     public void setVerticalThumbDrawable(Drawable thumb) {
         if (thumb != null) {
+            if (mMutated) {
+                thumb.mutate();
+            }
             thumb.setState(STATE_ENABLED);
             mVerticalThumb = thumb;
         }
@@ -198,6 +202,9 @@ public class ScrollBarDrawable extends Drawable {
 
     public void setVerticalTrackDrawable(Drawable track) {
         if (track != null) {
+            if (mMutated) {
+                track.mutate();
+            }
             track.setState(STATE_ENABLED);
         }
         mVerticalTrack = track;
@@ -205,6 +212,9 @@ public class ScrollBarDrawable extends Drawable {
 
     public void setHorizontalThumbDrawable(Drawable thumb) {
         if (thumb != null) {
+            if (mMutated) {
+                thumb.mutate();
+            }
             thumb.setState(STATE_ENABLED);
             mHorizontalThumb = thumb;
         }
@@ -212,6 +222,9 @@ public class ScrollBarDrawable extends Drawable {
 
     public void setHorizontalTrackDrawable(Drawable track) {
         if (track != null) {
+            if (mMutated) {
+                track.mutate();
+            }
             track.setState(STATE_ENABLED);
         }
         mHorizontalTrack = track;
@@ -225,6 +238,26 @@ public class ScrollBarDrawable extends Drawable {
             return mHorizontalTrack != null ? mHorizontalTrack.getIntrinsicHeight() :
                     mHorizontalThumb != null ? mHorizontalThumb.getIntrinsicHeight() : 0;
         }
+    }
+
+    @Override
+    public ScrollBarDrawable mutate() {
+        if (!mMutated && super.mutate() == this) {
+            if (mVerticalTrack != null) {
+                mVerticalTrack.mutate();
+            }
+            if (mVerticalThumb != null) {
+                mVerticalThumb.mutate();
+            }
+            if (mHorizontalTrack != null) {
+                mHorizontalTrack.mutate();
+            }
+            if (mHorizontalThumb != null) {
+                mHorizontalThumb.mutate();
+            }
+            mMutated = true;
+        }
+        return this;
     }
 
     @Override

VitaTaf/platform_frameworks_base

core/java/android/widget/ScrollBarDrawable.java
@@ -44,6 +44,7 @@ public class ScrollBarDrawable extends Drawable implements Drawable.Callback {
     private boolean mRangeChanged;
     private boolean mAlwaysDrawHorizontalTrack;
     private boolean mAlwaysDrawVerticalTrack;
+    private boolean mMutated;
 
     private int mAlpha = 255;
     private boolean mHasSetAlpha;
@@ -266,6 +267,10 @@ public class ScrollBarDrawable extends Drawable implements Drawable.Callback {
 
     private void propagateCurrentState(Drawable d) {
         if (d != null) {
+            if (mMutated) {
+                d.mutate();
+            }
+
             d.setState(getState());
             d.setCallback(this);
 
@@ -287,6 +292,26 @@ public class ScrollBarDrawable extends Drawable implements Drawable.Callback {
             return mHorizontalTrack != null ? mHorizontalTrack.getIntrinsicHeight() :
                     mHorizontalThumb != null ? mHorizontalThumb.getIntrinsicHeight() : 0;
         }
+    }
+
+    @Override
+    public ScrollBarDrawable mutate() {
+        if (!mMutated && super.mutate() == this) {
+            if (mVerticalTrack != null) {
+                mVerticalTrack.mutate();
+            }
+            if (mVerticalThumb != null) {
+                mVerticalThumb.mutate();
+            }
+            if (mHorizontalTrack != null) {
+                mHorizontalTrack.mutate();
+            }
+            if (mHorizontalThumb != null) {
+                mHorizontalThumb.mutate();
+            }
+            mMutated = true;
+        }
+        return this;
     }
 
     @Override